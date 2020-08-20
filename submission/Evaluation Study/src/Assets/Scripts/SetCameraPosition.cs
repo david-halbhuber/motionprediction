06c991d771133e681838f1195d4dfbccd90ad032
David Halbhuber <david.halbhuber@stud.uni-regensburg.de>
2020-08-21 01:45:19 +0200

    re-up

---
+++ submission/Evaluation Study/src/Assets/Scripts/SetCameraPosition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Das ist eine potentielle Lösung, um die Kamera in jedem Frame auf die Position des GameObjects, das sich zwischen den Augen befindet, zu setzen. Ich vermute, dass ihr das Problem mit der Kamera hattet,
+// weil euch SteamVR die Position der Kamera kontinuierlich überschrieben hat. --> Kamera befindet sich außerhalb der Szene. Da wird das InputTracking von Steam disablen und die Kamera manuell auf die Augenposition setzen,
+// wird lediglich die Rotation über das HMD geregelt.
+public class SetCameraPosition : MonoBehaviour
+{
+    // GameObject can be referenced in the Inspector (Create and place a GameObject in the center of the eyes)
+    public GameObject centerBetweenEyes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.XR.InputTracking.disablePositionalTracking = true;
+        transform.position = centerBetweenEyes.transform.position;
+        transform.rotation = centerBetweenEyes.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set Camera Position to Position of GameObject between eyes
+        transform.position = centerBetweenEyes.transform.position;
+        transform.rotation = centerBetweenEyes.transform.rotation;
+
+    }
+}
---
+++ submission/Evaluation Study/src/Assets/Scripts/StartQuestionnaire.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class StartQuestionnaire : MonoBehaviour
+{
+
+    public QuestionnaireControl questionnaireControl;
+    
+    void Start()
+    {
+        GetComponent<TextMeshPro>().faceColor = new Color32(23, 207, 214, 255);
+        gameObject.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        questionnaireControl.StartQuestionnaire();
+        gameObject.SetActive(false);
+    }
+}
